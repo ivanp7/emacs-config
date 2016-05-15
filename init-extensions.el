e5903bfa8cf0845bfe0298b79d4c831970c81195
ivanp7 <ivanp7@mail.ru>
2016-05-16 01:52:11 +0300

    Make exported LaTeX use russian babel package

--- init-extensions.el
+++ init-extensions.el
@@ -1,4 +1,6 @@
 ;;;; Org-mode
+(require 'org)
+
 (setq org-directory (default-value 'default-directory))
 (setq org-default-notes-file (concat org-directory "ivanp7.org"))
 (setq org-agenda-files (list (concat org-directory "ivanp7.org")))
@@ -38,6 +40,10 @@
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((lisp . t)))
+
+;; LaTeX
+
+(add-to-list 'org-latex-packages-alist '("russian" "babel"))
 
 ;;;; NLinum mode
 
@@ -666,8 +672,8 @@ Should be a list of the form ((MODE ((REGEXP . GLYPH) ...)) ...)"
                                 result-list
                                 (copy-to-list (tree-left-branch tree)
                                               (if (or (null exclude-p) (not (funcall
-                                                                          exclude-p
-                                                                          (tree-entry tree))))
+                                                                             exclude-p
+                                                                             (tree-entry tree))))
                                                   (cons (tree-entry tree)
                                                         (copy-to-list (tree-right-branch tree)
                                                                       result-list))
@@ -743,13 +749,13 @@ Should be a list of the form ((MODE ((REGEXP . GLYPH) ...)) ...)"
                     (let ((record (lookup-binary-tree sym rainbow-identifiers-custom-binary-tree)))
                       (if record
                           (setf (cdr record) (mod (+ rainbow-identifiers-tune-delta (cdr record))
-                                               rainbow-identifiers-face-count))
+                                                  rainbow-identifiers-face-count))
                           (setf rainbow-identifiers-custom-binary-tree
-                             (adjoin-binary-tree
-                              (cons sym (mod (+ rainbow-identifiers-tune-delta
-                                                (rainbow-identifiers--hash-function sym))
-                                             rainbow-identifiers-face-count))
-                              rainbow-identifiers-custom-binary-tree))))))))
+                                (adjoin-binary-tree
+                                 (cons sym (mod (+ rainbow-identifiers-tune-delta
+                                                   (rainbow-identifiers--hash-function sym))
+                                                rainbow-identifiers-face-count))
+                                 rainbow-identifiers-custom-binary-tree))))))))
         (font-lock-fontify-buffer))
       (message "Tune is not allowed in this mode.")))
 
@@ -764,8 +770,8 @@ Should be a list of the form ((MODE ((REGEXP . GLYPH) ...)) ...)"
                     (let ((record (lookup-binary-tree sym rainbow-identifiers-custom-binary-tree)))
                       (if record
                           (setf rainbow-identifiers-custom-binary-tree
-                             (delete-from-binary-tree
-                              (car record) rainbow-identifiers-custom-binary-tree))))))))
+                                (delete-from-binary-tree
+                                 (car record) rainbow-identifiers-custom-binary-tree))))))))
         (font-lock-fontify-buffer))
       (message "Tune is not allowed in this mode.")))
 
@@ -805,16 +811,16 @@ Should be a list of the form ((MODE ((REGEXP . GLYPH) ...)) ...)"
     (cond
       ((and (equal prev-char ?\|) (equal next-char ?\|)) t)
       ((or (and (= len 1) (equal first-char ?\.))
-          ;; (and (equal first-char ?\@) (equal prev-char ?\,))
-          (equal prefix2 "#\\") (equal prev-char ?\#)
-          (and (or (equal (upcase prefix11) "#<FUNCTION ")
-                (equal (upcase prefix17) "#<STANDARD-CLASS ")) (equal last-char ?\>))
-          (and (equal first-char ?\{) (equal prev-last-char ?\}) (equal last-char ?\>))
-          (member first-char '(?0 ?1 ?2 ?3 ?4 ?5 ?6 ?7 ?8 ?9))
-          (and (>= len 2) (member first-char '(?+ ?- ?\.))
-             (member second-char '(?0 ?1 ?2 ?3 ?4 ?5 ?6 ?7 ?8 ?9)))
-          (and (>= len 3) (member first-char '(?+ ?-)) (equal second-char ?\.)
-             (member third-char '(?0 ?1 ?2 ?3 ?4 ?5 ?6 ?7 ?8 ?9)))) nil)
+           ;; (and (equal first-char ?\@) (equal prev-char ?\,))
+           (equal prefix2 "#\\") (equal prev-char ?\#)
+           (and (or (equal (upcase prefix11) "#<FUNCTION ")
+                    (equal (upcase prefix17) "#<STANDARD-CLASS ")) (equal last-char ?\>))
+           (and (equal first-char ?\{) (equal prev-last-char ?\}) (equal last-char ?\>))
+           (member first-char '(?0 ?1 ?2 ?3 ?4 ?5 ?6 ?7 ?8 ?9))
+           (and (>= len 2) (member first-char '(?+ ?- ?\.))
+                (member second-char '(?0 ?1 ?2 ?3 ?4 ?5 ?6 ?7 ?8 ?9)))
+           (and (>= len 3) (member first-char '(?+ ?-)) (equal second-char ?\.)
+                (member third-char '(?0 ?1 ?2 ?3 ?4 ?5 ?6 ?7 ?8 ?9)))) nil)
       (t t))))
 
 (add-hook 'rainbow-identifiers-filter-functions 'rainbow-identifiers-filter)
@@ -844,7 +850,7 @@ Should be a list of the form ((MODE ((REGEXP . GLYPH) ...)) ...)"
 \(Unless it's a minibuffer window.)"
   (when hl-sexp-mode                    ; Could be made buffer-local.
     (unless (or (use-region-p)
-               (window-minibuffer-p (selected-window))) ; silly in minibuffer
+                (window-minibuffer-p (selected-window))) ; silly in minibuffer
       (unless hl-sexp-overlay
         (setq hl-sexp-overlay (make-overlay 1 1)) ; to be moved
         (overlay-put hl-sexp-overlay 'face 'hl-sexp-face))
@@ -880,8 +886,8 @@ Should be a list of the form ((MODE ((REGEXP . GLYPH) ...)) ...)"
   (interactive)
   (let* ((cb (char-before (point)))
          (matching-text (and cb
-                           (char-equal (char-syntax cb) ?\) )
-                           (blink-matching-open))))
+                             (char-equal (char-syntax cb) ?\) )
+                             (blink-matching-open))))
     (when matching-text (message matching-text))))
 
 ;;;; Paren face
@@ -896,9 +902,9 @@ Should be a list of the form ((MODE ((REGEXP . GLYPH) ...)) ...)"
 (setq highlight-symbol-on-navigation-p t)
 (setq highlight-symbol-idle-delay 0.2)
 (let ((hook (lambda () (interactive)
-               (cl-pushnew '(highlight-symbol-face :underline t)
-                           face-remapping-alist :test 'equal)
-               (highlight-symbol-mode))))
+                    (cl-pushnew '(highlight-symbol-face :underline t)
+                                face-remapping-alist :test 'equal)
+                    (highlight-symbol-mode))))
   (add-hook 'lisp-mode-hook hook)
   (add-hook 'emacs-lisp-mode-hook hook))
 
